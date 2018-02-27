1a1c0b10286d056f899f4cc395b7fd5606e4f44a
Peter S. Heijnen <heijnen@numdata.com>
2018-02-27 15:16:30 +0100

    Bug-fix: TextTools.isSecureFilePath() returns true for empty string.

--- commons/src/main/java/com/numdata/oss/TextTools.java
+++ commons/src/main/java/com/numdata/oss/TextTools.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, Numdata BV, The Netherlands.
+ * Copyright (c) 2018, Numdata BV, The Netherlands.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -2749,7 +2749,7 @@ public class TextTools
 		{
 			final int length = filePath.length();
 
-			result = true;
+			result = ( length > 0 );
 			int namePartLength = 0;
 			char previousCh = '\0';
 
